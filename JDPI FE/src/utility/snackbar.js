b004448dd2df9ce26c9b3e832ef118e1c4720186
Kavya Bhat <kavyarbhat66@gmail.com>
2024-10-05 20:07:10 +0530

    pushing snackbar changes

--- JDPI FE/src/utility/snackbar.js
+++ JDPI FE/src/utility/snackbar.js
@@ -3,10 +3,10 @@ import { enqueueSnackbar } from "notistack"
 
 const snackbar = (type, message)=>{
     if( type== 'success' || type== 'warning' || type== 'error' || type== 'info' ){
-    enqueueSnackbar(message, {variant:type, anchorOrigin:{vertical: 'top', horizontal: 'right'}})
+    enqueueSnackbar(message, {variant:type, anchorOrigin:{vertical: 'top', horizontal: 'right'}}, { autoHideDuration: 1000 })
     return;
     }
-    enqueueSnackbar(message, {variant:'default', anchorOrigin:{vertical: 'top', horizontal: 'right'}})
+    enqueueSnackbar(message, {variant:'default', anchorOrigin:{vertical: 'top', horizontal: 'right'}},{ autoHideDuration: 1000 })
 }
 
 export default snackbar;
