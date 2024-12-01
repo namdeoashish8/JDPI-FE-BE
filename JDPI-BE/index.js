c936ed9500e8808dde70df7241c6947f0d1faa8d
namdeoashish8 <namdeoashish8@gmail.com>
2024-12-01 19:16:57 +0530

    updated vercel json

--- JDPI-BE/index.js
+++ JDPI-BE/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
+    origin: ['http://localhost:5173', 'https://jdpi-fe-be.vercel.app']
 }))
 
 // Database Configuration
